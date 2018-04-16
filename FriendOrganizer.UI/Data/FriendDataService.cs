a1aa6062444c6f07261a8e10ad96568769c49e29
theFalconator <falcon.nick@gmail.com>
2018-04-16 17:51:36 -0500

    basic ui with dependency injection

---
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using FriendOrganizer.Model;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+        public IEnumerable<Friend> GetAllFriends()
+        {
+            yield return new Friend() {FirstName = "Thomas", LastName = "Huber"};
+            yield return new Friend() {FirstName = "Andreas", LastName = "Boehler"};
+            yield return new Friend() {FirstName = "Julia", LastName = "Huber"};
+            yield return new Friend() {FirstName = "Chrissi", LastName = "Egin"};
+        }
+    }
+}
---
+++ FriendOrganizer.UI/ViewModel/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.ObjectModel;
+using FriendOrganizer.Model;
+using FriendOrganizer.UI.Data;
+
+namespace FriendOrganizer.UI.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public ObservableCollection<Friend> Friends { get; set; }
+
+        public Friend SelectedFriend
+        {
+            get => _selectedFriend;
+            set
+            {
+                _selectedFriend = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private IFriendDataService friendDataService;
+        private Friend _selectedFriend;
+
+        public void Load()
+        {
+            var friends = friendDataService.GetAllFriends();
+            Friends.Clear();
+            foreach (var friend in friends)
+            {
+                Friends.Add(friend);
+            }
+        }
+
+        public MainViewModel(IFriendDataService friendDataService)
+        {
+            this.friendDataService = friendDataService;
+            Friends = new ObservableCollection<Friend>();
+        }
+    }
+}
